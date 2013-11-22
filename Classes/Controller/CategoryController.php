c498ca588dee229b404bd30d4f95090deefbe55c
Chr1stof <https://github.com/Chr1stof>
2013-11-22 10:44:37 +0100

    Testcommit

--- Classes/Controller/CategoryController.php
+++ Classes/Controller/CategoryController.php
@@ -19,7 +19,7 @@
 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *  GNU General Public License for more details.
-*
+*  test
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
